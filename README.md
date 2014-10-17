05b60e6e161f76c83ea2e54ec0abd910fd780290
Like FallingLeaves <likefallingleaves@gmail.com>
2014-10-16 20:51:07 -0700

    Init

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
 igheader
-========
+===========
 
-A simple sticky section header that has scroll behavior like in instagram
+This is a very small jquery plugin for a top bar which behaves like the top bar on Instagram with respect to scrolling. It basically allows for sticky section headers.
+
+API
+===
+
+The usage is very simple.  Add this to your HTML:
+
+```html
+   <script src="http://like-falling-leaves.github.io/igheader/igheader.jquery.js"><script>
+```
+
+and then invoke it like so:
+
+```javascript
+  $('div.myroot').igheader();
+```
+
+There are no options, no default styles.  Any entries with class *header* that are children of the root element provided are considered valid headers and are cloned and copied over to the fixed bar area.
+
+Styles for the fixed bar area can be provided by using the selector *div.bar .header* for example.  The plugin is automatically invoked on *.igheader* elements.
+
+The plugin listens for *scroll.igheader* jQuery events on the whole window.  If this is not the right place to listen for it, you can fix it by doing the following:
+
+```javascript
+
+  $(function () {
+     $(window).off('scroll.igheader'); // disable scroll events
+     $(some_element).on('scroll', function () {
+        $('div.myroot').trigger('igheader-update');
+     });
+  });
+
+```
+
+DEMO
+====
+
+Nothing is better than a live demo.  Click [here](http://like-falling-leaves.github.io/igheader/example.html) to see a demo.  If you are using Chrome on your desktop, emulate some devices and check it out.
+
+Take a look at the styles in the HTML head of the demo to figure out how to style the header for your usecase.
+
+MOBILE DEVICES
+==============
+
+This plugin obviously works on mobile devices but it requires [HammerJS](https://hammerjs.github.io/) to work well as mobile devices do not provide the right scroll events.  Take a look at the demo HTML to figure out how to include the required HammerJS code.
+
+This has not been tested with HammerJS v2.0 though it should be trivial to fix it if there are any issues.  Please report them or send patches.
---
+++ igheader.jquery.js
@@ -0,0 +1,124 @@
+//
+// jQuery plugin for IG-style fixed header bar
+// The structure is:
+// <div>...
+//   <div class="bar"></div>
+//   <div class="section">
+//    <div class="header">..</div>
+//   </div>
+// </div>
+//
+// This module clones the header and moves it to the 'bar' as needed.
+// No Styles are added here, look at examples to style headers.
+//
+
+;(function () {
+  var $ = window.jQuery || window.$;
+  $(function () {
+    addPlugin();
+    $('.igheader').igheader();
+  });
+
+  function addPlugin() {
+    $.fn.igheader = function (options) {
+      this.each(function () {
+        var data = $(this).data('igheader');
+        if (data) {
+          if (options == 'destroy') {
+            $(this).data('igheader', null);
+            $(this).off('.igheader').find('.bar').empty();
+            return;
+          }
+          return $(this).trigger('igheader-update');
+        } else {
+          if (options == 'destroy') return;
+          $(this).data('igheader', {});
+          setupHeader(this);
+        }
+      });
+      return this;
+    };
+  }
+
+  function setupHeader(elt) { 
+    var lastHeader = null, counter = 100;
+    var lastTopMargin = 0;
+    var headers;
+    
+    var bar = $(
+      $(elt).find('.bar')[0] || $('<div>', {'class': 'bar'}).appendTo($(elt))
+    );
+    
+    updateHeaders(); updateBarInfo();
+    $(window).on('scroll', updateBarInfo);
+    if (window.Hammer) {
+      var mc = new Hammer($(elt)[0], {pan: true, drag: true});
+      var delta = 0;
+      mc.on('panup pandown dragup dragdown panend dragend', function (ev) {
+        var deltaY = ('deltaY' in ev) ? ev.deltaY : (ev.gesture && ev.gesture.deltaY || 0);
+        $(elt).scrollTop($(elt).scrollTop() - deltaY + delta);
+        delta = deltaY;
+        updateBarInfo();
+      }).on('panend dragend', function () { delta = 0; });
+    }
+    return $(elt).on('igheader-update.igheader', function () { updateHeaders(); updateBarInfo(); })
+      .on('igheader-scroll.igheader', updateBarInfo)
+      .on('scroll.igheader', updateBarInfo);
+
+    function findCurrentElt(start) {
+      var top = $(elt).find('.bar')[0].getBoundingClientRect().top;
+      var candidate = null;
+      var index = start && headers.index(start) || 0;
+      while (1) {
+        if (index < 0) return null;
+        
+        var elt2 = headers.get(index);
+        if (!elt2) return candidate;
+        
+        var rect = elt2.getBoundingClientRect();
+        if (rect.top > top) {
+          if (candidate) return candidate; 
+          index --;
+        } else {
+          if (rect.bottom > top) return {elt: $(elt2), rect: rect, index: index};
+          candidate = {elt: $(elt2), index: index, rect: rect};
+          index ++;
+        }
+      }
+    }
+    
+    function updateBarInfo() {
+      var top = $(elt).find('.bar')[0].getBoundingClientRect().top;
+      var found = findCurrentElt(lastHeader);
+      if (!found) {
+        lastHeader = null;
+        lastTopMargin = 0;
+        bar.empty();
+        return;
+      }
+      
+      var newHeight = found.rect.height, newTopMargin = 0;
+      var nextHeader = $(headers.get(found.index + 1));
+      if (nextHeader.length) {
+        var rect = nextHeader[0].getBoundingClientRect();
+        if (rect.top < (top + newHeight)) newTopMargin = rect.top - newHeight - top; 
+      }
+      
+      if (found.elt[0] != lastHeader) {
+        lastHeader = found.elt[0];
+        bar.empty().append(found.elt.clone());
+        lastTopMargin = 0;
+      }
+      
+      if (lastTopMargin != newTopMargin) bar.find('.header').css({marginTop: newTopMargin});
+      lastTopMargin = newTopMargin;
+    }
+    
+    function updateHeaders() { headers = getHeaders(); }
+    function getHeaders() {
+      var barElt = $(bar).find('.header')[0];
+      return $($(elt).find('.header:visible').filter(function (index) {return (this !== barElt); }));
+    }
+  }
+})();
+
